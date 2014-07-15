8120dc7ed9a04d9bbf8db899ca470daae54d1f53
Anatol Broder <anatol@penibelst.de>
2014-07-15 10:09:07 +0200

    Add test for absent content and absent src

--- spec/html/proofer/scripts_spec.rb
+++ spec/html/proofer/scripts_spec.rb
@@ -8,10 +8,22 @@ describe "Scripts tests" do
     output.should match /External link http:\/\/www.asdo3IRJ395295jsingrkrg4.com\/asdo3IRJ.js? failed: 0 Couldn't resolve host name/
   end
 
+  it "works for valid internal src" do
+    file = "#{FIXTURES_DIR}/script_valid_internal.html"
+    output = capture_stderr { HTML::Proofer.new(file).run }
+    output.should == ""
+  end
+
   it "works for present content" do
     file = "#{FIXTURES_DIR}/script_content.html"
     output = capture_stderr { HTML::Proofer.new(file).run }
     output.should == ""
+  end
+
+  it "fails for absent content" do
+    file = "#{FIXTURES_DIR}/script_content_absent.html"
+    output = capture_stderr { HTML::Proofer.new(file).run }
+    output.should match /script has no src attribute/
   end
 
 end
