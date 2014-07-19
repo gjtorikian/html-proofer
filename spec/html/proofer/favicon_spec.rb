6e091ac9c96abbab96aa3aa56bf7fdb697b18392
Anatol Broder <anatol@penibelst.de>
2014-07-19 11:57:37 +0200

    Add specs

---
+++ spec/html/proofer/favicon_spec.rb
@@ -0,0 +1,22 @@
+require "spec_helper"
+
+describe "Favicon tests" do
+  it "ignores for absent favicon by default" do
+    absent = "#{FIXTURES_DIR}/favicon_absent.html"
+    output = capture_stderr { HTML::Proofer.new(absent).run }
+    output.should == ""
+  end
+
+  it "fails for absent favicon" do
+    absent = "#{FIXTURES_DIR}/favicon_absent.html"
+    output = capture_stderr { HTML::Proofer.new(absent, {:favicon => true}).run }
+    output.should match /no favicon included/
+  end
+
+  it "passes for present favicon" do
+    present = "#{FIXTURES_DIR}/favicon_present.html"
+    output = capture_stderr { HTML::Proofer.new(present, {:favicon => true}).run }
+    output.should == ""
+  end
+
+end
