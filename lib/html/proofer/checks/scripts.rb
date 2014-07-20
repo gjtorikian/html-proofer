9292c705b018dd69bb0b046e623510d7550323fa
Anatol Broder <anatol@penibelst.de>
2014-07-20 10:34:20 +0200

    Sooner next if ignore

--- lib/html/proofer/checks/scripts.rb
+++ lib/html/proofer/checks/scripts.rb
@@ -21,16 +21,15 @@ class Scripts < ::HTML::Proofer::Checks::Check
     @html.css('script').each do |s|
       script = Script.new s, "script", self
 
+      next if script.ignore?
       next unless script.blank?
 
       # does the script exist?
       if script.missing_src?
         self.add_issue "script is empty and has no src attribute"
       elsif script.remote?
-        next if script.ignore?
         add_to_external_urls script.src
       else
-        next if script.ignore?
         self.add_issue("internal script #{script.src} does not exist") unless script.exists?
       end
 
