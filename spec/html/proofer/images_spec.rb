9a30c370139354d89e6d03b1dc67daa49469b501
Garen Torikian <gjtorikian@gmail.com>
2014-07-20 12:55:01 -0700

    Rearrange tests for some organizational sanity

--- spec/html/proofer/images_spec.rb
+++ spec/html/proofer/images_spec.rb
@@ -1,56 +1,56 @@
 require "spec_helper"
 
-describe "Image tests" do
+describe "Images test" do
   it "passes for existing external images" do
-    externalImageFilepath = "#{FIXTURES_DIR}/existingImageExternal.html"
+    externalImageFilepath = "#{FIXTURES_DIR}/images/existingImageExternal.html"
     output = capture_stderr { HTML::Proofer.new(externalImageFilepath).run }
     output.should == ""
   end
 
   it "fails for image without alt attribute" do
-    missingAltFilepath = "#{FIXTURES_DIR}/missingImageAlt.html"
+    missingAltFilepath = "#{FIXTURES_DIR}/images/missingImageAlt.html"
     output = capture_stderr { HTML::Proofer.new(missingAltFilepath).run }
     output.should match /gpl.png does not have an alt attribute/
   end
 
   it "fails for image with an empty alt attribute" do
-    missingAltFilepath = "#{FIXTURES_DIR}/missingImageAltText.html"
+    missingAltFilepath = "#{FIXTURES_DIR}/images/missingImageAltText.html"
     output = capture_stderr { HTML::Proofer.new(missingAltFilepath).run }
     output.should match /gpl.png does not have an alt attribute/
   end
 
   it "fails for missing external images" do
-    externalImageFilepath = "#{FIXTURES_DIR}/missingImageExternal.html"
+    externalImageFilepath = "#{FIXTURES_DIR}/images/missingImageExternal.html"
     output = capture_stderr { HTML::Proofer.new(externalImageFilepath).run }
     output.should match /External link http:\/\/www.whatthehell\/? failed: 0 Couldn't resolve host/
   end
 
   it "fails for missing internal images" do
-    internalImageFilepath = "#{FIXTURES_DIR}/missingImageInternal.html"
+    internalImageFilepath = "#{FIXTURES_DIR}/images/missingImageInternal.html"
     output = capture_stderr { HTML::Proofer.new(internalImageFilepath).run }
     output.should match /doesnotexist.png does not exist/
   end
 
   it "fails for image with no src" do
-    imageSrcFilepath = "#{FIXTURES_DIR}/missingImageSrc.html"
+    imageSrcFilepath = "#{FIXTURES_DIR}/images/missingImageSrc.html"
     output = capture_stderr { HTML::Proofer.new(imageSrcFilepath).run }
     output.should match /image has no src attribute/
   end
 
   it "fails for image with default mac filename" do
-    terribleImageName = "#{FIXTURES_DIR}/terribleImageName.html"
+    terribleImageName = "#{FIXTURES_DIR}/images/terribleImageName.html"
     output = capture_stderr { HTML::Proofer.new(terribleImageName).run }
     output.should match /image has a terrible filename/
   end
 
   it 'ignores images marked as ignore data-proofer-ignore' do
-    ignorableImages = "#{FIXTURES_DIR}/ignorableImages.html"
+    ignorableImages = "#{FIXTURES_DIR}/images/ignorableImages.html"
     output = capture_stderr { HTML::Proofer.new(ignorableImages).run }
     output.should == ""
   end
 
   it 'properly checks relative images' do
-    relativeImages = "#{FIXTURES_DIR}/rootRelativeImages.html"
+    relativeImages = "#{FIXTURES_DIR}/images/rootRelativeImages.html"
     output = capture_stderr { HTML::Proofer.new(relativeImages).run }
     output.should == ""
 
@@ -60,20 +60,26 @@ describe "Image tests" do
   end
 
   it 'properly ignores data URI images' do
-    dataURIImage = "#{FIXTURES_DIR}/workingDataURIImage.html"
+    dataURIImage = "#{FIXTURES_DIR}/images/workingDataURIImage.html"
     output = capture_stderr { HTML::Proofer.new(dataURIImage).run }
     output.should == ""
   end
 
   it "works for valid images missing the protocol" do
-    missingProtocolLink = "#{FIXTURES_DIR}/image_missing_protocol_valid.html"
+    missingProtocolLink = "#{FIXTURES_DIR}/images/image_missing_protocol_valid.html"
     output = capture_stderr { HTML::Proofer.new(missingProtocolLink).run }
     output.should == ""
   end
 
   it "fails for invalid images missing the protocol" do
-    missingProtocolLink = "#{FIXTURES_DIR}/image_missing_protocol_invalid.html"
+    missingProtocolLink = "#{FIXTURES_DIR}/images/image_missing_protocol_invalid.html"
     output = capture_stderr { HTML::Proofer.new(missingProtocolLink).run }
     output.should match /404 No error/
+  end
+
+  it 'properly checks relative links' do
+    relativeLinks = "#{FIXTURES_DIR}/images/relativeToSelf.html"
+    output = capture_stderr { HTML::Proofer.new(relativeLinks).run }
+    output.should == ""
   end
 end
--- spec/html/proofer_spec.rb
+++ spec/html/proofer_spec.rb
@@ -3,10 +3,10 @@ require "spec_helper"
 describe HTML::Proofer do
   describe "#failed_tests" do
     it "is a list of the formatted errors" do
-      brokenLinkInternalFilepath = "#{FIXTURES_DIR}/brokenLinkInternal.html"
+      brokenLinkInternalFilepath = "#{FIXTURES_DIR}/links/brokenLinkInternal.html"
       proofer = HTML::Proofer.new(brokenLinkInternalFilepath)
       capture_stderr { proofer.run }
-      proofer.failed_tests.should eq(["\e[34mspec/html/proofer/fixtures/brokenLinkInternal.html\e[0m: internally linking to ./notreal.html, which does not exist"])
+      proofer.failed_tests.should eq(["\e[34mspec/html/proofer/fixtures/links/brokenLinkInternal.html\e[0m: internally linking to ./notreal.html, which does not exist", "\e[34mspec/html/proofer/fixtures/links/brokenLinkInternal.html\e[0m: internally linking to ./missingImageAlt.html, which does not exist"])
     end
   end
 end
