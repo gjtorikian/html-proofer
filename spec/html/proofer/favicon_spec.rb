f607ced35d95f64b5da03ded886bc346331e7408
Garen Torikian <gjtorikian@gmail.com>
2014-07-20 02:33:34 -0700

    Add explicit check for broken and ignorable favicons

--- spec/html/proofer/favicon_spec.rb
+++ spec/html/proofer/favicon_spec.rb
@@ -19,6 +19,12 @@ describe "Favicon tests" do
     output.should match /no favicon specified/
   end
 
+  it "fails for broken favicon" do
+    broken = "#{FIXTURES_DIR}/favicon_broken.html"
+    output = capture_stderr { HTML::Proofer.new(broken, {:favicon => true}).run }
+    output.should match /internally linking to asdadaskdalsdk.png/
+  end
+
   it "passes for present favicon" do
     present = "#{FIXTURES_DIR}/favicon_present.html"
     output = capture_stderr { HTML::Proofer.new(present, {:favicon => true}).run }
@@ -28,6 +34,12 @@ describe "Favicon tests" do
   it "passes for present favicon with shortcut notation" do
     present = "#{FIXTURES_DIR}/favicon_present_shortcut.html"
     output = capture_stderr { HTML::Proofer.new(present, {:favicon => true}).run }
+    output.should == ""
+  end
+
+  it "passes for broken favicon with data-proofer-ignore" do
+    broken_but_ignored = "#{FIXTURES_DIR}/favicon_broken_but_ignored.html"
+    output = capture_stderr { HTML::Proofer.new(broken_but_ignored, {:favicon => true}).run }
     output.should == ""
   end
 
