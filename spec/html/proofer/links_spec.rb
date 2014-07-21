838e8fad51cd19c254dd9fbe67c8910f2bf2a73c
Garen Torikian <gjtorikian@gmail.com>
2014-07-20 17:10:06 -0700

    Final slash may, or may not, be here (thanks Travis)

--- spec/html/proofer/links_spec.rb
+++ spec/html/proofer/links_spec.rb
@@ -169,6 +169,6 @@ describe "Links test" do
   it "works for array of links" do
     options = { :as_link_array => true}
     output = capture_stderr { HTML::Proofer.new(["www.github.com", "foofoofoo.biz"], options).run }
-    output.should match /foofoo.biz\/ failed: 0 Couldn't resolve host name/
+    output.should match /foofoo.biz\/? failed: 0 Couldn't resolve host name/
   end
 end
