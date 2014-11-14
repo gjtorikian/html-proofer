2ade840bd335b7de99cfc75344fd2b9179703f6c
Garen Torikian <gjtorikian@gmail.com>
2014-11-14 11:36:29 -0800

    Queue back the `effective_url`

--- lib/html/proofer.rb
+++ lib/html/proofer.rb
@@ -205,7 +205,7 @@ module HTML
       # just be lazy; perform an explicit get request. some servers are apparently not configured to
       # intercept HTTP HEAD
       elsif method == :head
-        queue_request(:get, href, filenames)
+        queue_request(:get, effective_url, filenames)
       else
         return if @options[:only_4xx] && !response_code.between?(400, 499)
         # Received a non-successful http response.
