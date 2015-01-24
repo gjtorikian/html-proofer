41558f4249ebff0070da30862b617402228c26bb
Anatol Broder <anatol@penibelst.de>
2015-01-24 13:44:48 +0100

    Wording

--- spec/html/proofer/utils_spec.rb
+++ spec/html/proofer/utils_spec.rb
@@ -3,11 +3,11 @@ require 'html/proofer/utils'
 
 describe HTML::Utils do
   describe '::create_nokogiri' do
-    it 'passes with a string' do
+    it 'passes for a string' do
       noko = HTML::Utils::create_nokogiri '<html lang="jp">'
       expect(noko.css('html').first['lang']).to eq 'jp'
     end
-    it 'passes with a file' do
+    it 'passes for a file' do
       noko = HTML::Utils::create_nokogiri "#{FIXTURES_DIR}/utils/lang-jp.html"
       expect(noko.css('html').first['lang']).to eq 'jp'
     end
