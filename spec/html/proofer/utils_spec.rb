7408a1b80d62357d5ccea3e4f641581a6e591757
Anatol Broder <anatol@penibelst.de>
2015-01-24 13:44:06 +0100

    Single quote style

--- spec/html/proofer/utils_spec.rb
+++ spec/html/proofer/utils_spec.rb
@@ -5,11 +5,11 @@ describe HTML::Utils do
   describe '::create_nokogiri' do
     it 'passes with a string' do
       noko = HTML::Utils::create_nokogiri '<html lang="jp">'
-      expect(noko.css("html").first["lang"]).to eq "jp"
+      expect(noko.css('html').first['lang']).to eq 'jp'
     end
     it 'passes with a file' do
       noko = HTML::Utils::create_nokogiri "#{FIXTURES_DIR}/utils/lang-jp.html"
-      expect(noko.css("html").first["lang"]).to eq "jp"
+      expect(noko.css('html').first['lang']).to eq 'jp'
     end
   end
 end
