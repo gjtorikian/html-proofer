d0c714ff7ab5f8f6d9ff9df320824e5f1904e55a
Garen Torikian <gjtorikian@gmail.com>
2015-08-11 16:17:11 -0700

    Add line numbers to failing HTML checks

--- spec/html/proofer/html_spec.rb
+++ spec/html/proofer/html_spec.rb
@@ -22,19 +22,19 @@ describe 'Html test' do
   it 'fails for an invalid tag' do
     html = "#{FIXTURES_DIR}/html/invalid_tag.html"
     proofer = run_proofer(html, { :check_html => true })
-    expect(proofer.failed_tests.first).to match(/Tag myfancytag invalid/)
+    expect(proofer.failed_tests.first).to match(/Tag myfancytag invalid \(line 2\)/)
   end
 
   it 'fails for an unmatched end tag' do
     html = "#{FIXTURES_DIR}/html/unmatched_end_tag.html"
     proofer = run_proofer(html, { :check_html => true })
-    expect(proofer.failed_tests.first).to match(/Unexpected end tag : div/)
+    expect(proofer.failed_tests.first).to match(/Unexpected end tag : div \(line 3\)/)
   end
 
   it 'fails for an unescaped ampersand in attribute' do
     html = "#{FIXTURES_DIR}/html/unescaped_ampersand_in_attribute.html"
     proofer = run_proofer(html, { :check_html => true })
-    expect(proofer.failed_tests.first).to match(/htmlParseEntityRef: expecting ';'/)
+    expect(proofer.failed_tests.first).to match(/htmlParseEntityRef: expecting ';' \(line 2\)/)
   end
 
   it 'fails for mismatch between opening and ending tag' do
@@ -46,12 +46,12 @@ describe 'Html test' do
   it 'fails for div inside head' do
     html = "#{FIXTURES_DIR}/html/div_inside_head.html"
     proofer = run_proofer(html, { :check_html => true })
-    expect(proofer.failed_tests.first).to match(/Unexpected end tag : head/)
+    expect(proofer.failed_tests.first).to match(/Unexpected end tag : head \(line 5\)/)
   end
 
   it 'fails for missing closing quotation mark in href' do
     html = "#{FIXTURES_DIR}/html/missing_closing_quotes.html"
     proofer = run_proofer(html, { :check_html => true })
-    expect(proofer.failed_tests.to_s).to match(/Couldn't find end of Start Tag a/)
+    expect(proofer.failed_tests.to_s).to match(/Couldn't find end of Start Tag a \(line 6\)/)
   end
 end
