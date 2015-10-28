81f46450c262c8290b04f28537fc83d728d65ca7
Garen Torikian <gjtorikian@gmail.com>
2015-10-27 17:42:46 -0700

    Unnecessary

--- lib/html/proofer.rb
+++ lib/html/proofer.rb
@@ -8,8 +8,6 @@ end
 require_all 'proofer'
 require_all 'proofer/check_runner'
 require_all 'proofer/checks'
-require_relative './proofer/utils'
-require_relative './proofer/xpathfunctions'
 
 require 'parallel'
 
