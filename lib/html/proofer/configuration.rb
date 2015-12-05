a557ade7391bef4d0cec984c83fe5f8d2385a2a0
Garen Torikian <gjtorikian@gmail.com>
2015-12-05 10:29:17 -0800

    Move configuration options to a new class

---
+++ lib/html/proofer/configuration.rb
@@ -0,0 +1,39 @@
+module HTML
+  class Proofer
+    class Configuration < Hash
+
+      PROOFER_DEFAULTS = {
+        :ext => '.html',
+        :check_favicon => false,
+        :href_swap => [],
+        :href_ignore => [],
+        :allow_hash_href => false,
+        :file_ignore => [],
+        :url_ignore => [],
+        :check_external_hash => false,
+        :alt_ignore => [],
+        :empty_alt_ignore => false,
+        :enforce_https => false,
+        :disable_external => false,
+        :verbose => false,
+        :only_4xx => false,
+        :directory_index_file => 'index.html',
+        :check_html => false,
+        :error_sort => :path,
+        :checks_to_ignore => []
+      }
+
+      TYPHOEUS_DEFAULTS = {
+        :followlocation => true,
+        :headers => {
+          'User-Agent' => "Mozilla/5.0 (compatible; HTML Proofer/#{VERSION}; +https://github.com/gjtorikian/html-proofer)"
+        }
+      }
+
+      HYDRA_DEFAULTS = {
+        :max_concurrency => 50
+      }
+
+    end
+  end
+end
