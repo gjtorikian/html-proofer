b0faf4a1ab73387ced95ef9e8bcecf8660639e69
Garen Torikian <gjtorikian@gmail.com>
2015-12-05 14:19:16 -0800

    Actually add `external_only` option

--- lib/html/proofer/configuration.rb
+++ lib/html/proofer/configuration.rb
@@ -9,19 +9,20 @@ module HTML
         :check_external_hash => false,
         :check_favicon => false,
         :check_html => false,
-        :checks_to_ignore => []
+        :checks_to_ignore => [],
         :directory_index_file => 'index.html',
         :disable_external => false,
         :empty_alt_ignore => false,
         :enforce_https => false,
         :error_sort => :path,
         :ext => '.html',
+        :external_only => false,
         :file_ignore => [],
         :href_ignore => [],
         :href_swap => [],
         :only_4xx => false,
         :url_ignore => [],
-        :verbose => false,
+        :verbose => false
       }
 
       TYPHOEUS_DEFAULTS = {
