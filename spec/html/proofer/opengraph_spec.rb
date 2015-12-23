23d5280189295b8911c86416f59c8bdd88cf6795
Garen Torikian <gjtorikian@gmail.com>
2015-12-22 19:09:51 -0600

    Add a fixed version of @penibelst's test

--- spec/html/proofer/opengraph_spec.rb
+++ spec/html/proofer/opengraph_spec.rb
@@ -6,19 +6,28 @@ describe 'Open Graph test' do
     proofer = run_proofer(url_valid, { :check_opengraph => true })
     expect(proofer.failed_tests).to eq []
   end
+
   it 'fails for empty url' do
     url_valid = "#{FIXTURES_DIR}/opengraph/url-empty.html"
     proofer = run_proofer(url_valid, { :check_opengraph => true })
     expect(proofer.failed_tests.first).to match /open graph is empty and has no content attribute/
   end
+
   it 'passes for existing external image' do
     url_valid = "#{FIXTURES_DIR}/opengraph/image-valid.html"
     proofer = run_proofer(url_valid, { :check_opengraph => true })
     expect(proofer.failed_tests).to eq []
   end
+
   it 'fails for missing external image' do
     url_valid = "#{FIXTURES_DIR}/opengraph/image-invalid.html"
     proofer = run_proofer(url_valid, { :check_opengraph => true })
     expect(proofer.failed_tests.first).to match /failed: 406/
+  end
+
+  it 'fails for missing internal images' do
+    image_internal_invalid = "#{FIXTURES_DIR}/opengraph/image-internal-invalid.html"
+    proofer = run_proofer(image_internal_invalid, { :check_opengraph => true })
+    expect(proofer.failed_tests.first).to match(/doesnotexist.png does not exist/)
   end
 end
