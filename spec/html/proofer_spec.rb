1de76d4fa26c66a27891f996aff67e7d184db826
Garen Torikian <gjtorikian@gmail.com>
2015-12-22 19:34:43 -0600

    Add `http_status_ignore` to ignore HTTP status codes

--- spec/html/proofer_spec.rb
+++ spec/html/proofer_spec.rb
@@ -134,4 +134,9 @@ describe HTML::Proofer do
       expect(proofer.failed_tests.length).to eq 1
     end
   end
+
+  it 'ignores status codes when asked' do
+    proofer = run_proofer(['www.github.com/github/notreallyhere'], :http_status_ignore => [404])
+    expect(proofer.failed_tests.length).to eq 0
+  end
 end
