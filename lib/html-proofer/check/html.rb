082f9bfa29958de1ff59dd345fd58ab4d6b4ea1d
Garen Torikian <gjtorikian@gmail.com>
2016-02-16 10:49:23 +0000

    Ignore namespaces in HTML validation

--- lib/html-proofer/check/html.rb
+++ lib/html-proofer/check/html.rb
@@ -1,13 +1,16 @@
 class HtmlCheck < ::HTMLProofer::Check
   SCRIPT_EMBEDS_MSG = /Element script embeds close tag/
-  INVALID_TAG_MSG = /Tag ([\w-]+) invalid/
+  INVALID_TAG_MSG = /Tag ([\w-:]+) invalid/
+  INVALID_PREFIX = /Namespace prefix/
 
   def run
     @html.errors.each do |error|
       message = error.message
       line    = error.line
 
-      next if !options[:validation][:report_invalid_tags] && message =~ INVALID_TAG_MSG
+      if message =~ INVALID_TAG_MSG || message =~ INVALID_PREFIX
+        next unless options[:validation][:report_invalid_tags]
+      end
 
       # tags embedded in scripts are used in templating languages: http://git.io/vOovv
       next if !options[:validation][:report_script_embeds] && message =~ SCRIPT_EMBEDS_MSG
