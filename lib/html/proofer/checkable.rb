a8dd3c256911f5d02852f1cfc8c1bf9924abee43
James Smith <james@floppy.org.uk>
2016-03-04 18:59:42 +0000

    add parentheses

--- lib/html/proofer/checkable.rb
+++ lib/html/proofer/checkable.rb
@@ -111,7 +111,7 @@ module HTML
 
         if path =~ %r{^/} # path relative to root
           base = File.directory?(@check.src) ? @check.src : File.dirname(@check.src)
-        elsif File.exist?(File.expand_path path, @check.src) || File.exist?(File.expand_path path_dot_ext, @check.src) # relative links, path is a file
+        elsif File.exist?(File.expand_path(path, @check.src)) || File.exist?(File.expand_path(path_dot_ext, @check.src)) # relative links, path is a file
           base = File.dirname @check.path
         elsif File.exist?(File.join(File.dirname(@check.path), path)) || File.exist?(File.join(File.dirname(@check.path), path_dot_ext)) # relative links in nested dir, path is a file
           base = File.dirname @check.path
