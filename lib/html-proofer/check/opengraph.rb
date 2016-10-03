2f46326fdaed3f898786bf36299fe61bcacbe3e3
Peter Newman <peterjnewman@gmail.com>
2016-10-03 03:14:03 +0100

    Try and fix the remaining syntax issues

--- lib/html-proofer/check/opengraph.rb
+++ lib/html-proofer/check/opengraph.rb
@@ -1,34 +1,34 @@
 # encoding: utf-8
 
-class OpenGraphCheckable < ::HTMLProofer::Checkable
-
+class OpenGraphCheck < ::HTMLProofer::Check
   def src
     @content
   end
+
 
   def missing_src?
     !src
   end
 
+
   def url
     src
   end
-end
 
-class OpenGraphCheck < ::HTML::Proofer::CheckRunner
+
   def run
     @html.css('meta[property="og:url"], meta[property="og:image"]').each do |m|
-      opengraph = OpenGraphCheckable.new m, self
+      @opengraph = create_element(m)
 
-      next if opengraph.ignore?
+      next if @opengraph.ignore?
 
       # does the opengraph exist?
-      if opengraph.missing_src?
+      if missing_src?
         add_issue('open graph is empty and has no content attribute', m.line)
-      elsif opengraph.remote?
+      elsif @opengraph.remote?
         add_to_external_urls opengraph.src, m.line
       else
-        add_issue("internal open graph #{opengraph.src} does not exist", m.line) unless opengraph.exists?
+        add_issue("internal open graph #{@opengraph.src} does not exist", m.line) unless @opengraph.exists?
       end
     end
 
