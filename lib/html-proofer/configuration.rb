c0c503d08a305eece03ee9d269765c9b519eb472
William Entriken <github.com@phor.net>
2017-03-17 13:45:51 -0400

    Comma

--- lib/html-proofer/configuration.rb
+++ lib/html-proofer/configuration.rb
@@ -33,7 +33,7 @@ module HTMLProofer
       :followlocation => true,
       :headers => {
         'User-Agent' => "Mozilla/5.0 (compatible; HTML Proofer/#{HTMLProofer::VERSION}; +https://github.com/gjtorikian/html-proofer)"
-      }
+      },
       :connecttimeout => 10
     }
 
