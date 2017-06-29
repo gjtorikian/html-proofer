84a7db78523538b48dc387ece3606512ce97a070
Stephen <stephengroat@users.noreply.github.com>
2017-06-28 17:15:30 -0700

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,9 @@
+require 'simplecov'
+SimpleCov.start
+if ENV['CI'] == 'true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
 require 'bundler/setup'
 require 'vcr'
 require 'timecop'
