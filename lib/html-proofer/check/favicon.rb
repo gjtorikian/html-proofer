88ad824b49bdfbda8c349e44e9440530af6dcbaf
James Smith <james@floppy.org.uk>
2017-09-14 18:59:08 +0100

    fix style check failures

--- lib/html-proofer/check/favicon.rb
+++ lib/html-proofer/check/favicon.rb
@@ -14,12 +14,12 @@ class FaviconCheck < ::HTMLProofer::Check
 
     add_issue('no favicon specified')
   end
-  
+
   private
-  
+
   def is_immediate_redirect?
     # allow any instant-redirect meta tag
-    @html.xpath("//meta[@http-equiv='refresh']").attribute('content').value.starts_with? "0;" rescue false
+    @html.xpath("//meta[@http-equiv='refresh']").attribute('content').value.starts_with? '0;' rescue false
   end
   
 end
