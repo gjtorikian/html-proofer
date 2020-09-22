1f1b750ad2438dc94fc048e74fc76cbf7105a784
Daniel Granerud <daniel.granerud@payex.com>
2020-09-22 07:42:49 +0200

    Trying to add urls to cache

--- lib/html-proofer/check/links.rb
+++ lib/html-proofer/check/links.rb
@@ -27,9 +27,9 @@ class LinkCheck < ::HTMLProofer::Check
     if @cache.use_cache?
       urls_to_check = load_cache
       check_links(urls_to_check)
-      @logger.log :info, @external_urls.inspect
+      @logger.log :info, "#{@external_urls.inspect}"
       @external_urls.each_key do |url|
-        @cache.add(url.to_s, @src, 200, "internal") unless @src.nil?
+        @cache.add url, @src, 200, "internal url"
       end
       @cache.write
     else
